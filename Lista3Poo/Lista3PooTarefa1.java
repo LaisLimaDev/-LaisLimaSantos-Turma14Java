e1dbc87aa15ac33c1e4beb70a849d76a7501ea2a
LaisLimaSantos <74927227+LaisLimaSantos@users.noreply.github.com>
2020-12-16 23:12:07 -0300

    Add files via upload

---
+++ Lista3Poo/Lista3PooTarefa1.java
@@ -0,0 +1,15 @@
+package POO;
+
+public class Lista3PooTarefa1 {
+	
+	String nome;
+	int idade;
+	
+	
+	
+	/*Crie uma hierarquia de classes conforme abaixo com os seguintes atributos e
+	comportamentos (observe a tabela), utilize os seus conhecimentos e distribua as
+	características de forma que tudo o que for comum a todos os animais fique na classe
+	Animal:*/
+
+}
